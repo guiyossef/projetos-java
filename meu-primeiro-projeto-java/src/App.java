5f37bd65141ea41f0ac5e5a26fb8750fcc75adfa
guiyossef <1yossefcontato@gmail.com>
2025-02-09 16:58:52 -0300

    primeiro projeto java

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Bom dia, amigos!!");
+        System.out.println("Bom dia, amigos!!!!");
     }
 }
